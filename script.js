3dbc9240a6ec028043c9859c8e3069c36892a8ee
Isabel de Aramburu <isabeldearamburu@MacBook-de-Isabel.local>
2019-06-21 21:59:12 -0300

    mas funciones

--- script.js
+++ script.js
@@ -49,31 +49,40 @@ const componentsSaleQuantity = component =>  {
     })
         return totalQuantity;
        }
+
+
+//4.VentasMes
+const monthSales = (month, year, data=store.sales) =>  {
+  let list = []
+  data.forEach(c => {
+      if (year === c.saleDate.getFullYear() && 
+          month===c.saleDate.getMonth() + 1) {
+          list.push(machinePrice(c.components))
+      }
+    })
+    let total = list.length ? list.reduce((a,b) => a+b) : 0
+    return total
+  }
+
+//  console.log( monthSales (1, 2019) );
+
           
 //console.log(componentsSaleQuantity("Monitor GPRS 3000"))
 
-/*
 // 3. vendedoraDelMes(mes, anio)
-const saleswomanOfTheMonth = (month, year) =>  {
-  let totalMonth= 0
-  let sales=store.sales
-  sales.forEach(c => {
-    let prices=store.prices
-    let yearSale=c.saleDate.getFullYear()
-    let monthSale=c.saleDate.getMonth() + 1
-      if (year === yearSale && month===monthSale) {
-        //totalMonth += c.sales
-        console.log(machinePrice(c.components));
-        let totalComponents=machinePrice(c.components)
-        let result = totalComponents.reduce(fmachinePrice(c.components))
-      }
-    })
-    //return totalMonth;
-  }
-  console.log(saleswomanOfTheMonth(1, 2019))
-*/
+const agentOfTheMonth =(m, y)=>{
+  let list = store.agents.map(
+    a => {
+      let x = { 
+        name:a, 
+        total: monthSales(m, y, store.sales.filter( s => s.agentName === a)) }
+        return x
+    }).reduce((a,b) => a.total > b.total ? a : b)
+    return list
+}
+//console.log(agentOfTheMonth(1,2019))
 
-
+/*
 //4. ventasMes (mes, anio)
 const totalMonthSales = (month, year) =>  {
   let totalMonth= 0
@@ -84,9 +93,56 @@ const totalMonthSales = (month, year) =>  {
     let monthSale=c.saleDate.getMonth() + 1
       if (year === yearSale && month===monthSale) {
         //totalMonth += c.sales
-        console.log(machinePrice(c.components));
-      }
-    })
-    return monthSales;
+        console.log(c.agentName);
 
-    console.log(totalMonthSales(1, 2019) )
+  }
+    console.log(totalMonthSales(1, 2019))
+
+// 5. ventasVendedora(nombre)
+
+const filteredSales = (type) => {
+
+  let filteredSales = 0
+
+  let sales = store.sales
+  sales.forEach(e => {
+    switch (type) {
+      case e.agentName:
+          return filteredSales += machinePrice(e.components)
+          break
+      case e.sucursal:
+      return filteredSales += machinePrice(e.components)
+          break
+    }
+  })
+
+  return filteredSales;
+}
+ 
+console.log (filteredSales("Caballito"));
+
+// 6. ComponenteMasVendido
+
+
+
+// componenteMasVendido() MAL
+
+const bestSeller = () => {
+  let prices = store.prices
+  let bestNumber = prices.map(e =>
+    componentSaleQuantity(e.component)).reduce((a, b) => a > b ? a : b)
+    prices.forEach(e =>{
+      if (bestNumber === componentSaleQuantity(e.component))) {e.component}
+}}
+  
+  }
+
+*/
+
+// 7.huboVentas
+
+const salesPositive = (month, year) =>  {
+  if (monthSales(month, year)> 0 )
+{return true} else {return false}
+}
+console.log(salesPositive(1,2019))
